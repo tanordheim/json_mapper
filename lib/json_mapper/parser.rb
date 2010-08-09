080b1cd3dce38c25d6c1bcceb6b426e8f9d6f0f8
Trond Arve Nordheim <tanordheim@gmail.com>
2010-08-09 18:57:57 +0200

    Return nil if there's no json data to parse

--- lib/json_mapper/parser.rb
+++ lib/json_mapper/parser.rb
@@ -3,6 +3,7 @@ module JSONMapper
 
     # Parse the JSON string into a Hash
     def self.parse(data)
+      return nil if data.nil? || data == ""
       JSON.parse(data, { :symbolize_names => true })
     end
 
